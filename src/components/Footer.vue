f5291db04490b24627f1eee55f3d4eb1e18036bc
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2023-01-20 19:52:54 +0000

    feat: add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    <p>Jacob Dunbar</p>
+  </footer>
+</template>
+
+<script></script>
+
+<style>
+footer {
+  width: 100%;
+  background-color: grey;
+  padding: 10px;
+}
+
+footer p {
+  text-align: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <Navbar />
     <Header />
     <MovieList />
-    <h1>Home</h1>
   </div>
 </template>
 
 <script>
-import Navbar from "../components/Navbar.vue";
 import Header from "../components/Header.vue";
 import MovieList from "../components/MovieList.vue";
 export default {
   name: "Home",
-  components: { Navbar, Header, MovieList },
+  components: { Header, MovieList },
 };
 </script>
