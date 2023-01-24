d77b6f8e83986d92f924b0ca01f9bb85cd203391
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2023-01-24 20:32:35 +0000

    feat: create and add logo to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,42 @@
 <template>
   <nav>
-    <RouterLink to="/"><h1>TMD</h1></RouterLink>
+    <RouterLink to="/"><Logo class="logo" /></RouterLink>
+    <p>Top Movie Database</p>
   </nav>
 </template>
 
-<script></script>
+<script>
+import Logo from "./Logo.vue";
+
+export default {
+  components: {
+    Logo,
+  },
+};
+</script>
 
 <style scoped>
 nav {
   width: 100%;
-  background: black;
-  padding-block: 15px;
+  background: #0f1116;
+
+  display: flex;
+  padding: 15px 75px 10px 75px;
+  align-items: center;
+  justify-content: space-between;
+  color: white;
+  opacity: 0.6;
+  text-transform: uppercase;
+  font-weight: 600;
+  font-size: 0.8rem;
 }
 
-nav h1 {
-  text-align: center;
-  font-size: 1rem;
-
-  color: white;
+.logo {
+  height: 30px;
+  opacity: 0.8;
+}
+.logo:hover {
+  height: 30px;
+  opacity: 1;
 }
 </style>
